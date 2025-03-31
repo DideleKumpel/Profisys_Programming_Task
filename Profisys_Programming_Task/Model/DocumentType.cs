a2cf09284ea3360ab2361f1d1c18aa1cba1bf722
Dawid Maćkiewicz <dawid2004m@gmail.com>
2025-04-01 01:46:48 +0200

    implementation of Documents Model

---
+++ Profisys_Programming_Task/Model/DocumentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profisys_Programming_Task.Model
+{
+    public enum DocumentType
+    {
+        Invoice,
+        Order,
+        Receipt
+    }
+}
---
+++ Profisys_Programming_Task/Model/Documents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Profisys_Programming_Task.Model
+{
+    internal class Documents
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DocumentType Type { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string City { get; set; }
+    }
+}
