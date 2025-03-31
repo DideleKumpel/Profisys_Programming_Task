9e4754c04591885a88ab0a6ce803c19666b05c2d
Dawid Maćkiewicz <dawid2004m@gmail.com>
2025-04-01 01:48:21 +0200

    implementation of DocumentItems

---
+++ Profisys_Programming_Task/Model/DocumentItems.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profisys_Programming_Task.Model
+{
+    internal class DocumentItems
+    {
+        [Key]
+        public int Id { get; set; }
+        //foreign key
+        [Required]
+        public int DocumentId { get; set; }
+        [Required]
+        public int Ordinal { get; set; }
+        [Required]
+        public string Product { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public int TaxRate { get; set; }
+
+        public Documents Document { get; set; }
+    }
+}
