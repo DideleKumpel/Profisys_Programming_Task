6ec8fc5b9c6234fd7b256613fa23a3488b77ee9d
Dawid Maćkiewicz <dawid2004m@gmail.com>
2025-04-01 02:23:54 +0200

    implementation of Main menu

--- Profisys_Programming_Task/ViewModel/MainMenuViewModel.cs
+++ Profisys_Programming_Task/ViewModel/MainMenuViewModel.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Profisys_Programming_Task.ViewModel
 {
-    internal class MainMenuViewModel
+    internal class MainMenuViewModel : ObservableObject
     {
+
+        public RelayCommand GoToImportViewCommand { get; }
+        public AsyncRelayCommand GoToDataViewCommand { get; }
+        public MainMenuViewModel()
+        {
+            GoToDataViewCommand = new AsyncRelayCommand(SwitchToDataView);
+            GoToImportViewCommand = new RelayCommand(SwitchToImportView);
+        }
+
+        private void SwitchToImportView()
+        {
+            Application.Current.MainWindow.DataContext = new ImportViewModel();
+        }
+
+        private async Task SwitchToDataView()
+        {
+            Application.Current.MainWindow.DataContext = new DataViewModel();
+        }
     }
 }
