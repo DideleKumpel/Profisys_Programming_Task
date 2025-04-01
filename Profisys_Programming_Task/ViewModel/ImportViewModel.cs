489aad6e1cbd6d7ced03f26d3cf2db83630b40e6
Dawid Maćkiewicz <dawid2004m@gmail.com>
2025-04-02 00:17:56 +0200

    Added properties and commands to ImportViewModel for handling data import.

--- Profisys_Programming_Task/ViewModel/ImportViewModel.cs
+++ Profisys_Programming_Task/ViewModel/ImportViewModel.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Profisys_Programming_Task.ViewModel
 {
-    internal class ImportViewModel
+    internal class ImportViewModel : ObservableObject
     {
+        //SERVICE
+        private readonly AppDbContext _appDbContext;
+
+        //DATA FOR IMPORT
+        public string PathString { get; set; }
+        public string IsImporting { get; set; }
+        public double ImportProgress { get; }
+
+        //COMMAND
+        public RelayCommand BackToMenuCommand { get; }
+        public RelayCommand ChooseFileCommand { get; }
+        public AsyncRelayCommand ImportDataCommand { get; }
+        public RelayCommand CancelImportCommand { get; }
+
+        //CONSTRUCTOR
+        public ImportViewModel(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+
+        }
+
     }
 }
