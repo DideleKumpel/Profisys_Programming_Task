eb765ebe56377c01729ca4161619dfb6bf73e940
Dawid Maćkiewicz <dawid2004m@gmail.com>
2025-04-01 10:43:46 +0200

    Added dataView UI working grid and back to menu button added enable button logic behind show docutment items

--- Profisys_Programming_Task/ViewModel/DataViewModel.cs
+++ Profisys_Programming_Task/ViewModel/DataViewModel.cs
@@ -1,12 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.EntityFrameworkCore;
+using Profisys_Programming_Task.Model;
 
 namespace Profisys_Programming_Task.ViewModel
 {
-    internal class DataViewModel
+    internal partial class DataViewModel : ObservableObject
     {
+        private readonly AppDbContext _appDbContext;
+        public ObservableCollection<Documents> _Documents { get; set; }
+
+        private Documents? _selectedDocument;
+        public Documents? SelectedDocument
+        {
+            get { return _selectedDocument; }
+            set
+            {
+                if (_selectedDocument != value)
+                {
+                    _selectedDocument = value;
+                    ShowDocumentDetailsCommand.NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+        public RelayCommand BackToMenuCommand { get; }
+        public RelayCommand ShowDocumentDetailsCommand { get; }
+        public DataViewModel(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _Documents = new ObservableCollection<Documents>();
+            BackToMenuCommand = new RelayCommand(SwitchToMainMenu);
+            ShowDocumentDetailsCommand = new RelayCommand(ShowDocumentDetails, CanShowDocumentDetails);
+        }
+
+        public async Task LoadDocuments()
+        {
+            _Documents = new ObservableCollection<Documents>(await _appDbContext.Documents.ToListAsync());
+            OnPropertyChanged(nameof(_Documents));
+        }
+
+        public void SwitchToMainMenu()
+        {
+            Application.Current.MainWindow.DataContext = new MainMenuViewModel();
+        }
+
+        private void ShowDocumentDetails()
+        {
+            if (_selectedDocument != null)
+            {
+                // to do
+            }
+        }
+        private bool CanShowDocumentDetails()
+        {
+            return _selectedDocument != null;
+        }
     }
 }
--- Profisys_Programming_Task/ViewModel/MainMenuViewModel.cs
+++ Profisys_Programming_Task/ViewModel/MainMenuViewModel.cs
@@ -11,13 +11,30 @@ namespace Profisys_Programming_Task.ViewModel
 {
     internal class MainMenuViewModel : ObservableObject
     {
-
+        private readonly AppDbContext _appDbContext;
         public RelayCommand GoToImportViewCommand { get; }
         public AsyncRelayCommand GoToDataViewCommand { get; }
         public MainMenuViewModel()
         {
+            _appDbContext = new AppDbContext();
             GoToDataViewCommand = new AsyncRelayCommand(SwitchToDataView);
             GoToImportViewCommand = new RelayCommand(SwitchToImportView);
+
+        }
+        public MainMenuViewModel(AppDbContext appDbContext)
+        {
+            try
+            {
+                _appDbContext = appDbContext;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Could not connect to the database. Please check your connection and try again.");
+                Application.Current.Shutdown();
+            }
+            GoToDataViewCommand = new AsyncRelayCommand(SwitchToDataView);
+            GoToImportViewCommand = new RelayCommand(SwitchToImportView);
+
         }
 
         private void SwitchToImportView()
@@ -27,7 +44,9 @@ namespace Profisys_Programming_Task.ViewModel
 
         private async Task SwitchToDataView()
         {
-            Application.Current.MainWindow.DataContext = new DataViewModel();
+            DataViewModel dataViewModel = new DataViewModel(_appDbContext);
+            await dataViewModel.LoadDocuments();
+            Application.Current.MainWindow.DataContext = dataViewModel;
         }
     }
 }
