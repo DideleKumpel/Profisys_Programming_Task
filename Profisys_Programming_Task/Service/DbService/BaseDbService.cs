c51af78be667b36a0aa73a4e563243da671b9ff8
Dawid Mackiewicz <dawid2004m@gmail.com>
2026-04-16 10:21:02 +0200

    Add interface and base class for Db operations

---
+++ Profisys_Programming_Task/Service/DbService/BaseDbService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profisys_Programming_Task.Service.DbService
+{
+    internal class BaseDbService<T> : IDbService<T>
+    {
+        public readonly AppDbContext _appDbContext;
+        public BaseDbService(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext;
+        }
+
+        public virtual void BeginTransaction()
+        {
+            _appDbContext.Database.BeginTransaction();
+        }
+
+        public virtual void CommitTransaction()
+        {
+            try
+            {
+                _appDbContext.SaveChanges();
+                _appDbContext.Database.CommitTransaction();
+            }
+            catch (Exception e)
+            {
+                RollbackTransaction();
+                throw e;
+            }
+        }
+        public virtual void RollbackTransaction()
+        {
+            _appDbContext.Database.RollbackTransaction();
+        }
+        public virtual T Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual int AddMany(List<T> items, bool CancelOnError)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual bool Delete(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual bool Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual T Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual T Update(int id, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual int UpdateMany(List<T> items, bool CancelOnError)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Profisys_Programming_Task/Service/DbService/IdbService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profisys_Programming_Task.Service.DbService
+{
+    internal interface IDbService<T>
+    {
+        public T GetById(int id);
+        public List<T> GetAll();
+        bool Exists(int id);
+
+        //Create
+        T Add(T item);
+        int AddMany(List<T> items, bool CancelOnError);
+
+        //Update
+        T Update(T item);
+        T Update(int id, T item);
+        int UpdateMany(List<T> items, bool CancelOnError);
+
+        //Delete
+        bool Delete(int id);
+        bool Delete(T item);
+
+        //Transations
+        void BeginTransaction();
+        void CommitTransaction();
+        void RollbackTransaction();
+
+    }
+}
