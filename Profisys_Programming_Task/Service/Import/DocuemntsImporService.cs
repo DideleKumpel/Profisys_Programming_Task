ae46f1c8b2792d9688a35ef42fb681ecb44449ad
Dawid Mackiewicz <dawid2004m@gmail.com>
2026-04-17 13:35:06 +0200

    Added IImportService

---
+++ Profisys_Programming_Task/Service/Import/DocuemntsImporService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Profisys_Programming_Task.Model;
+
+namespace Profisys_Programming_Task.Service.Import
+{
+    internal class DocuemntsImporService : IImportService<DocumentItems>
+    {
+
+        public DocuemntsImporService() { }
+
+        public List<DocumentItems> ImportDocumentsFromCsv(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<DocumentItems>> ImportDocumentsFromCsvAsync(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Profisys_Programming_Task/Service/Import/IImportService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profisys_Programming_Task.Service.Import
+{
+    internal interface IImportService<T> where T : class
+    {
+        public List<T> ImportDocumentsFromCsv(string filePath);
+        public Task<List<T>> ImportDocumentsFromCsvAsync(string filePath);
+    }
+}
